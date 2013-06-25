19b848a858c1c495f615b8c814386cbd694a3d40
radare <pancake@nopcode.org>
2013-06-25 17:13:59 +0200

    simplify code :P

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 function validate() {
     var pass = document.getElementsByName('password')[0].value;
-    var encoded = window.btoa(pass);
-    if (encoded == "c28gZWFzeQ==") 
+    if (pass == "so easy")
 	document.getElementById("answer").innerHTML += 'Congrats.<br>';
     else 
 	document.getElementById("answer").innerHTML += 'Gimme correct pass.<br>';
